3ce50a742cd928b8846c2c17d06457ccebf935a9
grinjk0418 <grinjk0418@gmail.com>
2025-09-12 17:34:09 +0900

    250912 [구현] FESTIVAL_LIST01 _ axios 처리까지 완료, 추후 랜더링 처리 필요

---
+++ src/configs/axiosConfig.js
@@ -0,0 +1,10 @@
+const axiosConfig = {
+  serviceKey: '0cbe7f01f9902708643e7690356749c42e0c68af9c1cc652308256a5e50edbbd',
+  MobileOS: 'WEB',
+  MobileApp: 'GreenTravel', 
+  type: 'json',
+  arrange: 'O',
+  baseUrl: 'https://apis.data.go.kr/B551011/KorService2',
+}
+
+export default axiosConfig;
